90ebd233e6b18ba4841fb02681e8770e0e6a0c54
abertrand <andrewbertrand@knights.ucf.edu>
2026-04-22 18:35:20 -0400

    Added background, defaulted shield to none, added legion name

--- radio_buttons.js
+++ radio_buttons.js
@@ -1,6 +1,8 @@
 let playerName = 'Player Name'
 nameType = 'Name_Effect_None'
 
+legionName = 'Legion Name'
+
 function updatePlayerName() {
     nameEffectElement = document.getElementById('name-effect-element')
     if (nameEffectElement != null) {
@@ -78,20 +80,13 @@ radioGroups.forEach(group => {
                 document.getElementById('shipimg').setAttribute('src', this.getAttribute('data-img'))
                 flairOverlay = document.getElementsByClassName('flair-effect-overlay')
                 console.log(this.getAttribute('data-img'))
-                flairOverlay[0].setAttribute('style', 'position: absolute; pointer-events: none; z-index: 10; left: 30px; top: 20px; width: 250px; height: 122px; background: linear-gradient(115deg, transparent 35%, rgba(255, 215, 0, 0.3) 45%, rgba(255, 255, 200, 0.5) 50%, rgba(255, 215, 0, 0.3) 55%, transparent 65%) 0% 0% / 300% 100%; animation: 11s linear 0s infinite normal none running flair-goldshimmer; mask-image: url(\"' + this.getAttribute('data-img') + '\"); mask-size: 100% 100%; mask-repeat: no-repeat;')
+                flairOverlay[0].setAttribute('style', 'position: absolute; pointer-events: none; z-index: 10; left: 40px; top: 40px; width: 250px; height: 122px; background: linear-gradient(115deg, transparent 35%, rgba(255, 215, 0, 0.3) 45%, rgba(255, 255, 200, 0.5) 50%, rgba(255, 215, 0, 0.3) 55%, transparent 65%) 0% 0% / 300% 100%; animation: 11s linear 0s infinite normal none running flair-goldshimmer; mask-image: url(\"' + this.getAttribute('data-img') + '\"); mask-size: 100% 100%; mask-repeat: no-repeat;')
 
             }
             else if (this.checked && this.getAttribute('name') == 'shield') {
                 shieldElement = document.getElementById('shield')
                 if (this.getAttribute('id') == 'Shield_None') {
                     shieldElement.remove()
-                    console.log('Removed')
-
-                    newElement = document.createElement('div')
-                    newElement.setAttribute('style', 'height: 230;width: 350;')
-                    newElement.setAttribute('id', "blank-shield")
-                    const wrapper = document.getElementById('flair-effect-wrapper')
-                    wrapper.after(newElement)
 
                 }
                 else if (shieldElement != null) {
@@ -99,10 +94,10 @@ radioGroups.forEach(group => {
                 }
                 else {
 
-                    blankShield = document.getElementById('blank-shield')
-                    blankShield.remove()
+                    //blankShield = document.getElementById('blank-shield')
+                    //blankShield.remove()
                     newElement = document.createElement('img')
-                    newElement.setAttribute('style', 'z-index:2; pointer-events:none; filter:brightness(150%);')
+                    newElement.setAttribute('style', 'position:absolute; left: 40px; top: 50px; z-index:2; pointer-events:none; filter:brightness(150%);')
                     newElement.setAttribute('width', '350')
                     newElement.setAttribute('src', this.getAttribute('data-img'))
                     newElement.setAttribute('id', 'shield')
@@ -126,4 +121,13 @@ const playerNameInput = document.getElementById('player-name-input')
 playerNameInput.addEventListener('input', function () {
     playerName = this.value
     updatePlayerName()
+})
+
+const legionNameInput = document.getElementById('legion-name-input')
+legionNameInput.addEventListener('input', function () {
+    legionName = this.value
+    element = document.getElementById('legion-name')
+    element.innerText = legionName + ' (60)'
+    console.log('trying')
+
 })
